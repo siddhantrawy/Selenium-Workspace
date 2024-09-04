95140240c38bb52a1ae734e92d4d3ed7bb54d3e3
Siddhant Rawy <siddhantrawy@gmail.com>
2024-09-04 05:43:12 +0530

    First Commit

---
+++ Selenium_POM_With_PageFactory/src/main/java/com/icolor/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package com.icolor.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	
+	WebDriver driver = null;
+	
+	// Constructor
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	// Locators
+	By loc1 = By.xpath("//input[@placeholder='Username']");
+	By loc2 = By.xpath("//input[@placeholder='Password']");
+	By loc3 = By.xpath("//button[@type='submit']");
+	
+	// Action Methods
+	public void setUserName(String user) {
+		driver.findElement(loc1).sendKeys(user);
+	}
+	
+	public void setPassword(String password) {
+		driver.findElement(loc2).sendKeys(password);
+	}
+	
+	public void clickLogin() {
+		driver.findElement(loc3).click();
+	}
+
+}
