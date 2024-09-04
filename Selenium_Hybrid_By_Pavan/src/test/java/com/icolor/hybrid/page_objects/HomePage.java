c24f5251ebd6045e2b15e17ee9d3b256e35bf906
Siddhant Rawy <siddhantrawy@gmail.com>
2024-09-04 05:54:49 +0530

    First Commit

---
+++ Selenium_Hybrid_By_Pavan/src/test/java/com/icolor/hybrid/page_objects/HomePage.java
@@ -0,0 +1,13 @@
+package com.icolor.hybrid.page_objects;
+
+import org.openqa.selenium.WebDriver;
+
+import com.icolor.hybrid.test_base.BasePage;
+
+public class HomePage extends BasePage {
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+
+}
---
+++ Selenium_Hybrid_By_Pavan/src/test/java/com/icolor/hybrid/page_objects/RegistrationPage.java
@@ -0,0 +1,5 @@
+package com.icolor.hybrid.page_objects;
+
+public class RegistrationPage {
+
+}
