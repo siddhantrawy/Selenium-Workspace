694d523beb9cda1cf6de47995f8f21029aa31e96
siddhant Rawy <siddhantrawy@gmail.com>
2024-09-20 02:17:15 +0530

    Third Commit

--- Selenium_WebDriver/src/main/java/com/icolor/webelements/CSS_Selector.java
+++ Selenium_WebDriver/src/main/java/com/icolor/webelements/CSS_Selector.java
@@ -1,9 +1,15 @@
 package com.icolor.webelements;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CSS_Selector {
 
 	public static void main(String[] args) {
 
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.opencart.com/");
+		
 		
 		
 	}
